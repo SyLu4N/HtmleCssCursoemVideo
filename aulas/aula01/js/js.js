358edd9edeff564209f328929d411a4f35d35510
Luan Carlos Simões <85505258+SyLu4N@users.noreply.github.com>
2021-12-13 22:48:16 -0300

    Initial commit

---
+++ aulas/aula01/js/js.js
@@ -0,0 +1,9 @@
+let ele = confirm("Podemos Nicoli?")
+if (ele == true){
+  console.log("Okay! Vamos continuar.");
+
+}
+
+else{
+  console.log("Xau!");
+}
---
+++ excercicios/ex01/js.js
@@ -0,0 +1,11 @@
+let eu = confirm("Deseja continuar?");
+if (eu == true){
+  console.log("Confirmado...")
+
+}
+
+else{
+
+  console.log("Okay, xau...")
+
+}
